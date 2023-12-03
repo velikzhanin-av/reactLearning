6545611df9a8aebdb761aff1b83a925c0f9e6128
velikzhanin-av <a.velikzhanin90@gmail.com>
2023-12-03 19:56:41 +0300

    init

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <div>
+            <h1>My Application</h1>
+            <a href="https://google.com" target="_blank">google</a>
+            <br/>
+            <a href="#">Home </a>
+            <a href="#">Monitoring </a>
+            <a href="#">Guide</a>
+
+        </div>
+    )
+}
+
+export default Header
